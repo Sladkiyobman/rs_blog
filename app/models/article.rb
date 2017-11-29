b3695f025cc97abece92ff3bfdcb4576341195ae
Sladkiyobman <masliy.grishka@gmail.com>
2017-11-29 18:27:29 +0200

    b7_29.11.17_18.27

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # Длина заголовка 5+ символов
   validates :title, presence:  true, length: {minimum: 5}
 
